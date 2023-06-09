9cfc9f352263148204c01fa0559814fbddfd1b3a
Eduardo Franco <franc0edu24@gmail.com>
2023-06-08 22:48:04 -0300

    finished method of authentication

--- src/app/components/login-screen/login-screen.component.ts
+++ src/app/components/login-screen/login-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-login-screen',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class LoginScreenComponent {
 
+  Login!: FormGroup;
+  username: string = '';
+  password: string = '';
+  constructor(public service: HospitalService, private formBuilder: FormBuilder, private router: Router){}
+
+  ngOnInit(): void{
+    this.Login = this.formBuilder.group({
+      login: [this.username, Validators.compose([
+        Validators.required,
+      ])],
+      password: [this.password, Validators.compose([
+        Validators.required
+      ])]
+    })
+  }
+
+  async AuthenticationMethod(): Promise<void>{
+    if(this.Login.valid){
+      try{
+        const result = await this.service.login(this.username,this.password)
+        this.router.navigate(['/listHospitals']);
+      }catch(error){
+        console.log(error)
+      }
+    }
+  }
 }
