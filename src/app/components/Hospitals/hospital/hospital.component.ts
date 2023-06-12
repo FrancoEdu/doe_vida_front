285e750d7796520817430959380390fce29af728
Eduardo Franco <franc0edu24@gmail.com>
2023-06-12 15:24:55 -0300

    Refactor architecture

--- src/app/components/Hospitals/hospital/hospital.component.ts
+++ src/app/components/Hospitals/hospital/hospital.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Hospital } from '../../interface/Hospital';
+import { Hospital } from '../../../interface/Hospital';
 import { States } from 'src/app/enums/states';
 
 @Component({
